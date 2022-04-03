e2b9eaea1ae6af8a455373a21aa0cbc2ac938dc1
Franco-gmz <fdgomez@fi.uba.ar>
2022-04-03 17:37:08 -0300

    add file testing and move example files into assets folder

--- src/file.rs
+++ src/file.rs
@@ -8,9 +8,34 @@ pub fn read_file_lines(filename: &str) -> Result<Vec<String>, String> {
         Ok(readlines) => readlines,
         Err(_) => return Err("No se pudo abrir el archivo".to_string()),
     };
-    let mut lines: Vec<&str> = lines.split('\n').collect();
-    for line in lines.iter_mut() {
-        content.push(line.to_string());
+    if lines != ""{
+        let mut lines: Vec<&str> = lines.split('\n').collect();
+        for line in lines.iter_mut() {
+            content.push(line.to_string());
+        }
     }
     Ok(content)
 }
+
+#[cfg(test)]
+mod test{
+    use crate::file::read_file_lines;
+
+
+    #[test]
+    fn couldnt_open(){
+        let non_existent_file = "name";
+        let expected_message = "No se pudo abrir el archivo".to_string();
+        assert_eq!(Err(expected_message), read_file_lines(&non_existent_file));
+    }
+
+    #[test]
+    fn empty_file(){
+        let empty_file_path = "./assets/empty.txt";
+        let expected_length = 0;
+        let lines = read_file_lines(&empty_file_path).unwrap();
+        println!("{:?}", lines);
+        assert_eq!(expected_length, lines.len());
+    }
+}
+
