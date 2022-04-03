5923ce83693961c15d1062dd30a0103478ac979a
Franco-gmz <fdgomez@fi.uba.ar>
2022-04-03 15:15:19 -0300

    Change File "class" to function read_file_lines to comply requirements

--- src/file.rs
+++ src/file.rs
@@ -1,27 +1,16 @@
 use std::fs;
 
-///A structure which contains the content of a file by lines
-pub struct File {
-    lines: Vec<String>,
-}
-
-impl File {
-    /// It receives a path to a file, opens it and saves the content. If the opening fails, it returns a
-    /// message as String.
-    pub fn new(filename: &str) -> Result<Self, String> {
-        let mut content = Vec::new();
-        let lines: String = match fs::read_to_string(filename) {
-            Ok(readlines) => readlines,
-            Err(_) => return Err("No se pudo abrir el archivo".to_string()),
-        };
-        let mut lines: Vec<&str> = lines.split('\n').collect();
-        for line in lines.iter_mut() {
-            content.push(line.to_string());
-        }
-        Ok(File { lines: content })
+/// It receives a path to a file, opens it and returns the content in a vector by lines.
+/// If the opening fails, it returns a message as String.
+pub fn read_file_lines(filename: &str) -> Result<Vec<String>, String> {
+    let mut content = Vec::new();
+    let lines: String = match fs::read_to_string(filename) {
+        Ok(readlines) => readlines,
+        Err(_) => return Err("No se pudo abrir el archivo".to_string()),
+    };
+    let mut lines: Vec<&str> = lines.split('\n').collect();
+    for line in lines.iter_mut() {
+        content.push(line.to_string());
     }
-    ///It returns the content of the opened file.
-    pub fn content(&self) -> Vec<String> {
-        self.lines.clone()
-    }
+    Ok(content)
 }
--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,8 @@ fn main() -> Result<(), String> {
     let filename1: &String = &args.arg1;
     let filename2: &String = &args.arg2;
     /* read files */
-    //let content1: Vec<String> = file::read(file1);
-    //let content2: Vec<String> = file::read(file2);
-    let file1: file::File = file::File::new(filename1)?;
-    let file2: file::File = file::File::new(filename2)?;
-    let content1: Vec<String> = file1.content();
-    let content2: Vec<String> = file2.content();
+    let content1: Vec<String> = file::read_file_lines(filename1)?;
+    let content2: Vec<String> = file::read_file_lines(filename2)?;
     /* diff */
     let grid: Vec<Vec<i32>> = diff::lcs(&content1, &content2);
     let length1 = content1.len();
